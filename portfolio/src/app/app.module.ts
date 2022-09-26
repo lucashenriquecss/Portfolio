c380391bf2a30f18fbf705e3a6f6512ec3cc0a24
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-09-25 23:29:54 -0300

    Estrutura do knowledge  feita

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { AboutComponent } from './components/about/about.component';
+import { KnowledgeComponent } from './components/knowledge/knowledge.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonsComponent,
-    AboutComponent
+    AboutComponent,
+    KnowledgeComponent
   ],
   imports: [
     BrowserModule
