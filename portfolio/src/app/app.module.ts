26f52932ec6a35c8bbb9a387b91b05de72767156
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-10-18 10:33:16 -0300

    Mudança nos componentes

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
-import { ButtonsComponent } from './components/buttons/buttons.component';
 import { AboutComponent } from './components/about/about.component';
 import { KnowledgeComponent } from './components/knowledge/knowledge.component';
 import { CardKnowledgeComponent } from './components/card-knowledge/card-knowledge.component';
 import { GithubComponent } from './components/github/github.component';
 import { CardGithubComponent } from './components/card-github/card-github.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonsComponent,
     AboutComponent,
     KnowledgeComponent,
     CardKnowledgeComponent,
@@ -20,7 +20,10 @@ import { CardGithubComponent } from './components/card-github/card-github.compon
     CardGithubComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
